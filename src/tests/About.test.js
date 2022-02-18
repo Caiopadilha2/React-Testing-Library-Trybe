c81817dd5069566727af86f344afa27024396d5a
Caio <caiopadilha@id.uff.br>
2022-02-18 17:02:36 -0300

    Criando o renderWithRouter e finalizando req 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
-import App from '../App';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
 
 // test('Teste se a página contém as informações sobre a Pokédex.', () => {
 
 // });
 
 test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
-  render(<App />);
+  const { history } = renderWithRouter(<About />);
+  history.push('/about');
 
-  const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+  const title = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
 
   expect(title).toBeInTheDocument();
 });
 
 test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-  
-    const parag1 = screen.getByRole('article',
-    { name: /This application simulates a Pokédex/i });
+  const { history } = renderWithRouter(<About />);
+  history.push('/about');
+
+  const parag1 = screen.getByRole('article',
+    { name: 'This application simulates a Pokédex, a digital encyclopedia containing all Pokémons' });
   const parag2 = screen.getByRole('article',
-    { name: /One can filter Pokémons by type/i });
+    { name: 'One can filter Pokémons by type, and see more details for each one of them'});
 
   expect(parag1).toBeInTheDocument();
   expect(parag2).toBeInTheDocument();
 });
 
 test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
-  
-    const logoEl = screen.getByRole('img', { name: 'Pokédex' });
+  const { history } = renderWithRouter(<About />);
+  history.push('/about');
+
+  const logoEl = screen.getByRole('img', { name: 'Pokédex' });
   expect(logoEl).toBeInTheDocument();
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de nav.', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/');
+
+  const home = screen.getByRole('link', { name: 'Home' });
+  expect(home).toBeInTheDocument();
+  const about = screen.getByRole('link', { name: 'About' });
+  expect(about).toBeInTheDocument();
+  const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(favorites).toBeInTheDocument();
+});
