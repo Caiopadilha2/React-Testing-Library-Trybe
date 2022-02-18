99d655690666e887eeba58f7e020b646330b84a1
Caio <caiopadilha@id.uff.br>
2022-02-18 15:51:47 -0300

    Iniciando req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import App from '../App';
+
+// test('Teste se a página contém as informações sobre a Pokédex.', () => {
+
+// });
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+  render(<App />);
+
+  const title = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+  expect(title).toBeInTheDocument();
+});
+
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  
+    const parag1 = screen.getByRole('article',
+    { name: /This application simulates a Pokédex/i });
+  const parag2 = screen.getByRole('article',
+    { name: /One can filter Pokémons by type/i });
+
+  expect(parag1).toBeInTheDocument();
+  expect(parag2).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+  
+    const logoEl = screen.getByRole('img', { name: 'Pokédex' });
+  expect(logoEl).toBeInTheDocument();
+});
