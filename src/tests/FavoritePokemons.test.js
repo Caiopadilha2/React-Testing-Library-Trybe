d78b8b6fe5521b1e3efe952e103386c5763c783c
Caio <caiopadilha@id.uff.br>
2022-02-19 02:39:53 -0300

    Implementa primeiro teste req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const emptyPokemons = screen.getByText('No favorite pokemon found');
+  expect(emptyPokemons).toBeInTheDocument();
+});
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+});
