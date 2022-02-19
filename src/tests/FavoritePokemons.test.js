907c306455c7bdd596e4369e4091efc4c1b9a811
Caio <caiopadilha@id.uff.br>
2022-02-19 17:16:16 -0300

    Req 3 adiciona um pokemon e vê se ele está nos favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { FavoritePokemons } from '../components';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
   renderWithRouter(<FavoritePokemons />);
@@ -9,5 +11,21 @@ test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
   expect(emptyPokemons).toBeInTheDocument();
 });
 
-test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+  renderWithRouter(<App />);
+
+  const buttonMoreDetails = screen.getByRole('link', { name: /More details/i });
+  expect(buttonMoreDetails).toBeInTheDocument();
+  userEvent.click(buttonMoreDetails);
+
+  const buttonFavoritar = screen.getByRole('checkbox', { name: /pokémon favoritado/i });
+  expect(buttonFavoritar).toBeInTheDocument();
+  userEvent.click(buttonFavoritar);
+
+  const buttonFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  expect(buttonFavorites).toBeInTheDocument();
+  userEvent.click(buttonFavorites);
+
+  const pokemonsFavoritos = screen.getByRole('img', { name: /marked as favorite/ });
+  expect(pokemonsFavoritos).toBeInTheDocument();
 });
